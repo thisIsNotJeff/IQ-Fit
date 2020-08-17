cc84dc4d663ea63d05766350ab5737349b4681c9
Steve Blackburn <steve.blackburn@anu.edu.au>
2020-08-17 22:19:00 +1000

    Initial import

---
+++ src/comp1110/ass2/FitGame.java
@@ -0,0 +1,91 @@
+package comp1110.ass2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class provides the text interface for the IQ Fit Game
+ * <p>
+ * The game is based directly on Smart Games' IQ-Fit game
+ * (https://www.smartgames.eu/uk/one-player-games/iq-fit)
+ */
+public class FitGame {
+
+    /**
+     * Determine whether a piece placement is well-formed according to the
+     * following criteria:
+     * - it consists of exactly four characters
+     * - the first character is a valid piece descriptor character (b, B, g, G, ... y, Y)
+     * - the second character is in the range 0 .. 9 (column)
+     * - the third character is in the range 0 .. 4 (row)
+     * - the fourth character is in valid orientation N, S, E, W
+     *
+     * @param piecePlacement A string describing a piece placement
+     * @return True if the piece placement is well-formed
+     */
+    static boolean isPiecePlacementWellFormed(String piecePlacement) {
+        return false; // FIXME Task 2: determine whether a piece placement is well-formed
+    }
+
+    /**
+     * Determine whether a placement string is well-formed:
+     * - it consists of exactly N four-character piece placements (where N = 1 .. 10);
+     * - each piece placement is well-formed
+     * - no shape appears more than once in the placement
+     * - the pieces are ordered correctly within the string
+     *
+     * @param placement A string describing a placement of one or more pieces
+     * @return True if the placement is well-formed
+     */
+    public static boolean isPlacementWellFormed(String placement) {
+        return false; // FIXME Task 3: determine whether a placement is well-formed
+    }
+
+    /**
+     * Determine whether a placement string is valid.
+     *
+     * To be valid, the placement string must be:
+     * - well-formed, and
+     * - each piece placement must be a valid placement according to the
+     *   rules of the game:
+     *   - pieces must be entirely on the board
+     *   - pieces must not overlap each other
+     *
+     * @param placement A placement string
+     * @return True if the placement sequence is valid
+     */
+    public static boolean isPlacementValid(String placement) {
+        return false; // FIXME Task 5: determine whether a placement string is valid
+    }
+
+    /**
+     * Given a string describing a placement of pieces, and a location
+     * that must be covered by the next move, return a set of all
+     * possible next viable piece placements which cover the location.
+     *
+     * For a piece placement to be viable it must:
+     *  - be a well formed piece placement
+     *  - be a piece that is not already placed
+     *  - not overlap a piece that is already placed
+     *  - cover the location
+     *
+     * @param placement A starting placement string
+     * @param col      The location's column.
+     * @param row      The location's row.
+     * @return A set of all viable piece placements, or null if there are none.
+     */
+    static Set<String> getViablePiecePlacements(String placement, int col, int row) {
+        return null; // FIXME Task 6: determine the set of all viable piece placements given existing placements
+    }
+
+    /**
+     * Return the solution to a particular challenge.
+     **
+     * @param challenge A challenge string.
+     * @return A placement string describing the encoding of the solution to
+     * the challenge.
+     */
+    public static String getSolution(String challenge) {
+        return null;  // FIXME Task 9: determine the solution to the game, given a particular challenge
+    }
+}
---
+++ src/comp1110/ass2/gui/Board.java
@@ -0,0 +1,23 @@
+package comp1110.ass2.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+
+    private static final int BOARD_WIDTH = 933;
+    private static final int BOARD_HEIGHT = 700;
+
+    // FIXME Task 7: Implement a basic playable Fix Game in JavaFX that only allows pieces to be placed in valid places
+
+    // FIXME Task 8: Implement challenges (you may use assets provided for you in comp1110.ass2.gui.assets)
+
+    // FIXME Task 10: Implement hints (should become visible when the user presses '/' -- see gitlab issue for details)
+
+    // FIXME Task 11: Generate interesting challenges (each challenge may have just one solution)
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
