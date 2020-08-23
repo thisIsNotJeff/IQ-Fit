161006977a0253aebcdb4f90fb8c3ad3c12b4b2f
BoYang Gao <u7175363@anu.edu.au>
2020-08-24 01:49:59 +0800

    editing.

--- src/comp1110/ass2/Puzzle.java
+++ src/comp1110/ass2/Puzzle.java
@@ -1,16 +1,17 @@
 package comp1110.ass2;
 
 public class Puzzle {
-    final private Direction direction;
-    final private Color color;
+    public Direction direction;
+    public Color color;
     public int[][] position;
 
     /**
      * Constructor.
      * @param direction the orientation of this puzzle piece
      * @param color the color used to represent this puzzle piece
+     * @param position the position of this puzzle is placed
      */
-    Puzzle(Direction direction, Color color, int[][] position){
+    public Puzzle(Direction direction, Color color, int[][] position){
         this.direction=direction;
         this.color=color;
         this.position=position;
