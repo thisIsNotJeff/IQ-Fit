19062d54e3ebaa6e7783524894f097302ec40b19
BoYang Gao <u7175363@anu.edu.au>
2020-08-24 00:44:53 +0800

    editing.

--- src/comp1110/ass2/Color.java
+++ src/comp1110/ass2/Color.java
@@ -7,7 +7,8 @@ package comp1110.ass2;
  */
 
 public enum Color {
-    BLUE('B'), GREEN('G'), INDIGO('I'), LIMEGREEN('L'), NAVYBLUE('N'), PINK('P'), RED('R'), SKYBLUE('S'), YELLOW('Y');
+    BLUE('B'), GREEN('G'), INDIGO('I'), LIMEGREEN('L'), NAVYBLUE('N'), PINK('P'), RED('R'), SKYBLUE('S'), YELLOW('Y'),
+    blue('b'), green('g'), indigo('i'), limegreen('l'), navyblue('n'), pink('p'), red('r'), skyblue('s'), yellow('y');
 
     final private char value;
 
--- src/comp1110/ass2/Puzzle.java
+++ src/comp1110/ass2/Puzzle.java
@@ -27,3 +27,5 @@ public class Puzzle {
     public int[][] getPosition(){return position;}
 
 }
+
+
