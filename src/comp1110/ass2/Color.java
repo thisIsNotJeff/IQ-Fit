027a9491587eb2f4ddfb23c33a6bb03e7229f5e5
BoYang Gao <u7175363@anu.edu.au>
2020-08-25 10:21:17 +0800

    upstream pull

--- src/comp1110/ass2/Color.java
+++ src/comp1110/ass2/Color.java
@@ -10,7 +10,8 @@ public enum Color {
     BLUE('B'), GREEN('G'), INDIGO('I'), LIMEGREEN('L'), NAVYBLUE('N'), PINK('P'), RED('R'), SKYBLUE('S'), YELLOW('Y'),
     blue('b'), green('g'), indigo('i'), limegreen('l'), navyblue('n'), pink('p'), red('r'), skyblue('s'), yellow('y');
 
-    final private char value;
+    char value;
+
 
     Color(char value){ this.value=value;}
 
