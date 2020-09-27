510245b7412e4ab85737ea2a827d240747677aca
Yuxuan Hu <u7167529@anu.edu.au>
2020-09-27 21:32:08 +0800

    test program

--- tests/comp1110/ass2/CanBePutTest.java
+++ tests/comp1110/ass2/CanBePutTest.java
@@ -29,56 +29,90 @@ public class CanBePutTest {
             }
         }
         PuzzlePieces p1 = new PuzzlePieces(Direction.EAST, Color.blue, 5, 0);
-        test(p1, pos, false); // invalid row
-        PuzzlePieces p2 = new PuzzlePieces(Direction.EAST, Color.blue, 0, 10);
-        test(p2, pos, false); // invalid column
-        PuzzlePieces p3 = new PuzzlePieces(Direction.EAST, Color.blue, 0, 0);
-        test(p3, pos, true); // invalid column
+        test(p1, pos, false);
+        PuzzlePieces p2 = new PuzzlePieces(Direction.WEST, Color.green, 0, 10);
+        test(p2, pos, false);
+        PuzzlePieces p3 = new PuzzlePieces(Direction.SOUTH, Color.ORANGE, -1, 0);
+        test(p3, pos, false);
+        PuzzlePieces p4 = new PuzzlePieces(Direction.WEST, Color.SKYBLUE, 0, -2);
+        test(p4, pos, false);
     }
-/*
+
     @Test
-    public void piecesWellFormed() {
-        for (int i = 0; i < SOLUTIONS.length; i++)
-            for (int j = 4; j < SOLUTIONS[i].placement.length(); j += 4) {
-                test(SOLUTIONS[i].placement.substring(0, j), true);
-                test(SOLUTIONS[i].placement.substring(0, j - 4) + BAD_PIECES[i % BAD_PIECES.length], false);
+    public void invalidPieces() {
+        int[][] pos = new int[10][5];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 5; j++) {
+                pos[i][j] = 0;
             }
+        }
+        PuzzlePieces p1 = new PuzzlePieces(Direction.EAST, Color.blue, 5, 0);
+        test(p1, pos, false);
+        PuzzlePieces p2 = new PuzzlePieces(Direction.EAST, Color.blue, 0, 10);
+        test(p2, pos, false);
+        PuzzlePieces p3 = new PuzzlePieces(Direction.EAST, Color.blue, -1, 0);
+        test(p3, pos, false);
+        PuzzlePieces p4 = new PuzzlePieces(Direction.EAST, Color.blue, 0, -2);
+        test(p4, pos, false);
     }
 
     @Test
-    public void correctOrder() {
-        for (int i = 0; i < SOLUTIONS.length; i++) {
-            test(SOLUTIONS[i].placement, true);
-            String wrong = SOLUTIONS[i].placement.substring(SOLUTIONS[i].placement.length() / 2);
-            wrong += SOLUTIONS[i].placement.substring(0, SOLUTIONS[i].placement.length() / 2);
-            test(wrong, false);
+    public void offBoard() {
+        int[][] pos = new int[10][5];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 5; j++) {
+                pos[i][j] = 0;
+            }
         }
+        PuzzlePieces p1 = new PuzzlePieces(Direction.EAST, Color.blue, 2, 7);
+        test(p1, pos, false);
+        PuzzlePieces p2 = new PuzzlePieces(Direction.WEST, Color.RED, 0, 9);
+        test(p2, pos, false);
+        PuzzlePieces p3 = new PuzzlePieces(Direction.SOUTH, Color.limegreen, 4, 2);
+        test(p3, pos, false);
+        PuzzlePieces p4 = new PuzzlePieces(Direction.EAST, Color.blue, 4, 0);
+        test(p4, pos, false);
+    }
+    @Test
+    public void overlap() {
+        int[][] pos = new int[10][5];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 5; j++) {
+                pos[i][j] = 0;
+            }
+        }
+        pos[0][0] = pos[1][0] = pos[2][0] = pos[3][0] = pos[0][1] = 1;
+        PuzzlePieces p1 = new PuzzlePieces(Direction.NORTH, Color.orange, 0, 3);
+        test(p1, pos, false);
+        pos[1][1] = pos[2][1] = pos[3][1] = pos[4][1] = pos[3][2] = pos[4][2] = 1;
+        PuzzlePieces p2 = new PuzzlePieces(Direction.SOUTH, Color.skyblue, 1, 3);
+        test(p2, pos, false);
+        PuzzlePieces p3 = new PuzzlePieces(Direction.EAST, Color.LIMEGREEN, 2, 2);
+        test(p3, pos, false);
+        pos[5][2] = pos[6][2] = pos[7][2] = pos[8][2] = pos[5][3] = 1;
+        PuzzlePieces p4 = new PuzzlePieces(Direction.SOUTH, Color.INDIGO, 4, 3);
+        test(p4, pos, false);
     }
 
     @Test
-    public void duplicatesA() {
-        for (int i = 0; i < 40; i += 4) {
-            if (i == 16)
-                continue;
-            String bad = SOLUTIONS[100].placement.substring(0, i) +
-                    SOLUTIONS[100].placement.substring(16, 20) +
-                    SOLUTIONS[100].placement.substring(i + 4, 40);
-            test(SOLUTIONS[i + 32].placement, true);
-            test(bad, false);
+    public void validPosition() {
+        int[][] pos = new int[10][5];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 5; j++) {
+                pos[i][j] = 0;
+            }
         }
+        pos[0][0] = pos[1][0] = pos[2][0] = pos[3][0] = pos[0][1] = 1;
+        pos[5][2] = pos[6][2] = pos[7][2] = pos[8][2] = pos[5][3] = 1;
+        PuzzlePieces p1 = new PuzzlePieces(Direction.NORTH, Color.YELLOW, 1, 1);
+        test(p1, pos, true);
+        pos[8][0] = pos[9][0] = pos[6][1] = pos[7][1] = pos[8][1] = pos[9][1] = 1;
+        PuzzlePieces p2 = new PuzzlePieces(Direction.NORTH, Color.orange, 0, 4);
+        test(p2, pos, true);
+        pos[2][2] = pos[1][3] = pos[2][3] = pos[3][3] = 1;
+        PuzzlePieces p3 = new PuzzlePieces(Direction.WEST, Color.LIMEGREEN, 2, 0);
+        test(p3, pos, true);
+        PuzzlePieces p4 = new PuzzlePieces(Direction.EAST, Color.navyblue, 2, 8);
+        test(p4, pos, true);
     }
-
-    @Test
-    public void duplicatesB() {
-        for (int i = 0; i < 40; i += 4) {
-            if (i == 16)
-                continue;
-            String bad = SOLUTIONS[100].placement.substring(0, i) +
-                    SOLUTIONS[100].placement.substring(16, 20).charAt(0) +
-                    "00W" +
-                    SOLUTIONS[100].placement.substring(i + 4, 40);
-            test(SOLUTIONS[i + 32].placement, true);
-            test(bad, false);
-        }
-    }*/
 }
