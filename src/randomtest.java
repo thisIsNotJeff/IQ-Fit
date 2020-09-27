c41baa2fad9db073eca0b3658061948139b382d5
BoYang Gao <u7175363@anu.edu.au>
2020-09-27 23:38:13 +0800

    D2E

--- src/randomtest.java
+++
@@ -1,7 +0,0 @@
-import comp1110.ass2.Games;
-
-public class randomtest {
-    public static void main(String[] args) {
-        System.out.println(Games.getDifficulty(new Games(1, "B03SG70Si52SL00Nn01Er41WS40Ny62N", "B03SG70Si52SL00Nn01Eo63Sp20Er41WS40Ny62N")));
-    }
-}
