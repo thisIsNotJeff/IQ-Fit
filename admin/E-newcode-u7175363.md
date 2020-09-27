c350899d5fa3a8f7236c2b7be0df95b5d706981d
BoYang Gao <u7175363@anu.edu.au>
2020-09-27 23:37:21 +0800

    D2E

--- admin/E-newcode-u7175363.md
+++ admin/E-newcode-u7175363.md
@@ -4,5 +4,7 @@
 
 For Deliverable D2E, I contributed the following new statements of original code:
 
-- Added the [newGames()](https://gitlab.cecs.anu.edu.au/u7133046/comp1110-ass2-tue15g/-/blob/master/src/comp1110/ass2/Games.java#L170-178) function to Games class
+- Added the [newGames()](https://gitlab.cecs.anu.edu.au/u7133046/comp1110-ass2-tue15g/-/blob/master/src/comp1110/ass2/Games.java#L167-185) function to Games class
+- Added the [getDifficulty()](https://gitlab.cecs.anu.edu.au/u7133046/comp1110-ass2-tue15g/-/blob/master/src/comp1110/ass2/Games.java#L206-213) function to Games class
+- Added the [equals()](https://gitlab.cecs.anu.edu.au/u7133046/comp1110-ass2-tue15g/-/blob/master/src/comp1110/ass2/Games.java#L192-200) function to Games class
 - Added the [NewGamesTest](https://gitlab.cecs.anu.edu.au/u7133046/comp1110-ass2-tue15g/-/blob/master/tests/comp1110/ass2/NewGamesTest.java) class.
--- tests/comp1110/ass2/NewGamesTest.java
+++ tests/comp1110/ass2/NewGamesTest.java
@@ -1,39 +1,45 @@
 package comp1110.ass2;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertTrue;
 
 public class NewGamesTest {
 
     private void testDifficulty(int difficulty) {
-        Games[] out = new Games[24];
-        for (int i = 0; i < out.length; i++) {
-            out[i] = Games.newGames(difficulty);
-            int diff = Games.getDifficulty(out[i]);
-            assertTrue("Expected difficulty " + difficulty + ", but " + (diff == -1 ? "did not get one from the pre-defined games" : "got one of difficulty " + diff) + ": problem number " + out[i].getNumber() + ".", diff == difficulty);
+
+        // create 1000 games with newGames method, and test if all these games are having right difficulty level
+        ArrayList<Games> out = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            out.add(i,Games.newGames(difficulty));
+            int diff = Games.getDifficulty(out.get(i));
+            assertTrue("Expected difficulty " + difficulty + ", but " + (diff == -1 ? "did not get one from the pre-defined games" : "got one of difficulty " + diff) + ".", diff == difficulty);
         }
-        int unique = Games.countGames(out);
-        assertTrue("Expected at least 3 different objectives after calling newGames() 24 times, but only got " + unique + ".", unique >= 3);
+
+
+        ArrayList<Games> distinct = new ArrayList<>(out.size());
+        for(int i =0; i < out.size();i++){
+            if(!distinct.contains(out.get(i))){
+                distinct.add(out.get(i));
+            }
+        }
+        assertTrue("Expected all 24 distinct games from difficulty " + difficulty + ", but only got " + distinct.size() + ".", distinct.size() == 24);
     }
 
     @Test
-    public void testStarter() {
-        testDifficulty(0);
-    }
+    public void testStarter() { testDifficulty(0);}
 
     @Test
-    public void testJunior() {
-        testDifficulty(1);
-    }
+    public void testJunior() { testDifficulty(1);}
 
     @Test
-    public void testExpert() {
-        testDifficulty(2);
-    }
+    public void testExpert() { testDifficulty(2);}
 
     @Test
-    public void testMaster() { testDifficulty(3); }
+    public void testMaster() { testDifficulty(3);}
 
     @Test
-    public void testWizard() { testDifficulty(4); }
+    public void testWizard() { testDifficulty(4);}
 }
